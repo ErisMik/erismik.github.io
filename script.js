9da2c4359d9341208f5171c0a79e06d4d80eddaf
Eric Mikulin <ericm99@gmail.com>
2017-07-19 00:55:06 -0700

    Made it grab the bitbucket repos aswell

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Make the github AJAX call for my Repos
 $.getJSON("https://api.github.com/users/erismik/repos", function(data) {
     var items = [];
     $.each(data, function(key, val) {
@@ -7,6 +8,24 @@ $.getJSON("https://api.github.com/users/erismik/repos", function(data) {
             "<td>"+val["description"]+"</td>"+
             "<td>"+val["language"]+"</td>"+
             "<td>"+val["updated_at"]+"</td>"+
+            "</tr>"
+        );
+    });
+
+    $("#current table").append(items.join(""));
+});
+
+// Make the bitbucket AJAX call for my Repos
+$.getJSON("https://api.bitbucket.org/2.0/repositories/itsEris", {origin: "http://bitbucket.com"}, function(data) {
+    var items = [];
+    var stuff = data["values"];
+    $.each(stuff, function(key, val) {
+        items.push(
+            "<tr>"+
+            "<td><a href=\""+val["links"]["html"]["href"]+"\">"+val["name"]+"</a></td>"+
+            "<td>"+val["description"]+"</td>"+
+            "<td>"+val["language"]+"</td>"+
+            "<td>"+val["updated_on"]+"</td>"+
             "</tr>"
         );
     });
